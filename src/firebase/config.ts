c6587a09924c7b039d37c69d114a5bf99f78f406
Alpha Tapfuma <alphatapfuma362a@gmail.com>
2025-10-01 21:59:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4654281853-a56f4",
+  "appId": "1:1062419582019:web:19a099f3b328293f4b18f6",
+  "apiKey": "AIzaSyAYOMqvZBiyGugIYn4Y-g1RS4ZM1k0C9g8",
+  "authDomain": "studio-4654281853-a56f4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1062419582019"
+};
